5a0e6247c50cfcad3991be94e8dd6e7031737010
Tadele Melese <109243986+tadele-melese@users.noreply.github.com>
2024-07-31 17:29:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 Machine Learning App ')
 
 st.info('This app builds machine learning model')
 with st.expander('Data'):
   st.write('**Main Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/tadele-melese/Machine_Learning-App1/master/climate.csv')
+  df
